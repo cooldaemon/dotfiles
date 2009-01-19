0829e3a7012d5d6c97c2ea938c1bcd69e1e605e2
cooldaemon <cooldaemon@gmail.com>
2009-01-19 14:54:37 +0900

    Fixed.

--- bin/js/jslint.js
+++ bin/js/jslint.js
@@ -1,27 +1,30 @@
 load(environment.HOME + '/bin/js/fulljslint.js');
-
-var file   = arguments[0];
-var source = arguments[1];
-
-if (!file || !source) {
-  print('Usage: jslint.js [File Name] [Source]');
-  quit(1);
-}
-
-if (JSLINT(source, {passfail: false})) {
-  print('All good.');
-  quit();
-}
-
-for (var i = 0; i < JSLINT.errors.length; i += 1) {
-  var e = JSLINT.errors[i];
-  if (e) {
-    print(
-      file + ':' +
-      (e.line + 1) + ':' +
-      (e.character + 1) + ': ' +
-      e.reason
-    );
+(function (file, source) {
+  if (!file || !source) {
+    print('Usage: jslint.js [File Name] [Source]');
+    quit(1);
   }
-}
+
+  if (JSLINT(source, {passfail: false})) {
+    print('All good.');
+    quit();
+  }
+
+  JSLINT.errors.forEach(function (error) {
+    var message;
+    [
+      file,
+      error.line + 1,
+      error.character + 1,
+      error.reason
+    ].forEach(function (elem) {
+      if (message) {
+        message += ':' + elem;
+      } else {
+        message = elem;
+      }
+    });
+    print(message);
+  });
+})(arguments[0], arguments[1]);
 
