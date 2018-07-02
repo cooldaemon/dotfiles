cbc5120d70f34f24f2041b08065582868f51d814
cooldaemon <cooldaemon@gmail.com>
2018-07-02 11:32:56 +0900

    Updated config files.
    - Compile options for Erlang.
    - Init env for Fish.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
+set -gx PATH ~/bin $PATH
 set -gx OMF_PATH $HOME/.local/share/omf
 set -gx EDITOR vi
 set -gx fish_escape_delay_ms 10
 set fish_theme agnoster
 
 source $HOME/.config/fish/alias.fish
-source $HOME/.config/fish/anyenv.fish
+#source $HOME/.config/fish/anyenv.fish
