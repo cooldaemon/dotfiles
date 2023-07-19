f817c4b64730172c15d35f2a1af07cbed682ff18
cooldaemon <cooldaemon@gmail.com>
2023-07-19 16:06:29 +0900

    Add env vals for installing erlang by asdf.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,16 @@
 set -gx fish_escape_delay_ms 10
 source $HOME/.config/fish/alias.fish
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if test -f (brew --prefix asdf)/libexec/asdf.fish
+  source (brew --prefix asdf)/libexec/asdf.fish
+end
+
+if test -d (brew --prefix openjdk)/bin
+  set -gx PATH (brew --prefix openjdk)/bin $PATH
+end
+
+set -x KERL_CONFIGURE_OPTIONS --with-ssl=(brew --prefix openssl@3) --with-odbc=(brew --prefix unixodbc)
+set -x CC /usr/bin/gcc -I(brew --prefix unixodbc)/include
+set -x LDFLAGS -L(brew --prefix unixodbc)/lib
