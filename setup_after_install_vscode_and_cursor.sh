1fdcd3fdd77016d3ae49c0d3f9cbb271fe2e1d04
cooldaemon <cooldaemon@gmail.com>
2024-03-12 19:12:09 +0900

    Added settings for VSCode and Cursor.

--- setup_after_install_vscode_and_cursor.sh
+++ setup_after_install_vscode_and_cursor.sh
@@ -1,6 +1,8 @@
 # For VS Code
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+cp ./settings.json ~/Library/Application\ Support/Code/User/settings.json
 
 # For Cursor editor
 appId=$(osascript -e 'id of app "Cursor"')
 defaults write "$appId" ApplePressAndHoldEnabled -bool false
+cp ./settings.json ~/Library/Application\ Support/Cursor/User/settings.json
